6447cffca3d20e78287bfa3b3b730f88d6904d30
moliveri1618 <lastiada1@gmail.com>
2025-02-28 21:14:28 +0000

    s

--- api/models/items.py
+++ api/models/items.py
@@ -1,1 +1,6 @@
-# define models for items
+from sqlmodel import SQLModel, Field
+
+class Item(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    type: str = Field(..., nullable=False)
+    owner: str = Field(..., nullable=False)
