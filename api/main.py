96dd16a956eefa5ce12f850f63ecd37919ea2ce2
moliveri1618 <lastiada1@gmail.com>
2025-02-28 21:57:26 +0000

    v

--- api/main.py
+++ api/main.py
@@ -3,9 +3,8 @@ import os
 from fastapi import FastAPI
 from mangum import Mangum
 from fastapi.middleware.cors import CORSMiddleware
-from sqlmodel import create_engine
-from sqlalchemy.exc import OperationalError
-from sqlalchemy import text
+from sqlmodel import SQLModel, create_engine
+from contextlib import asynccontextmanager
 
 if os.getenv("GITHUB_ACTIONS"):sys.path.append(os.path.dirname(__file__)) 
 from routers import items  
@@ -14,19 +13,16 @@ from routers import items
 rds_postgresql_url = "postgresql://rootuser:password@fastapi-aws-database.cjo4ss2ailsb.eu-north-1.rds.amazonaws.com:5432/postgres"
 engine = create_engine(rds_postgresql_url, echo=True)
 
-def test_db_connection():
-    try:
-        with engine.connect() as connection:
-            connection.execute(text("SELECT 1"))  # ✅ Use `text()`
-        print("✅ Database connection successful!")
-    except OperationalError as e:
-        print("❌ Database connection failed:", str(e))
+# Initialize the database
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+    
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()  # ✅ Runs only when the app starts
+    yield
 
-# Run this before initializing tables
-test_db_connection()
-
-
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 handler = Mangum(app=app)
 
 # CORS 
