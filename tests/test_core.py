4207f1b1cfbda99808a13fe26e40b1311305a2a1
moliveri1618 <lastiada1@gmail.com>
2025-02-28 00:02:42 +0000

    d

--- tests/test_core.py
+++ tests/test_core.py
@@ -6,4 +6,4 @@ client = TestClient(app)
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello Worlddfdfdf"}
+    assert response.json() == {"message": "Hello asdasd"}
