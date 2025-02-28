09f4fd5b2feaa23673d60cad31d9cdab5bdd6ecc
moliveri1618 <lastiada1@gmail.com>
2025-02-28 01:29:19 +0000

    c

--- api/main.py
+++ api/main.py
@@ -2,13 +2,11 @@ from fastapi import FastAPI
 from mangum import Mangum
 import sys
 import os
-# Dynamically adjust the import path
-if os.getenv("GITHUB_ACTIONS"):
-    sys.path.append(os.path.dirname(__file__))  # Ensure current dir is in path
+from fastapi.middleware.cors import CORSMiddleware
 
+if os.getenv("GITHUB_ACTIONS"):sys.path.append(os.path.dirname(__file__)) 
 from routers import items  # Works both locally and in GitHub Actions
 
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 handler = Mangum(app=app)
@@ -16,7 +14,7 @@ handler = Mangum(app=app)
 # CORS 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Change this in production!
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
