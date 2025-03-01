13090dae831ce86492c1e77717706e1e3f11d154
moliveri1618 <lastiada1@gmail.com>
2025-03-01 22:07:32 +0000

    d

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,7 +1,9 @@
 from fastapi.testclient import TestClient
 from api.main import app
+from api.dependecies import verify_cognito_token
 
 client = TestClient(app)
+app.dependency_overrides[verify_cognito_token] = lambda: {"sub": "test-user", "username": "testuser"}
 
 def test_root():
     response = client.get("/")
