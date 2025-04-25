8c9795d3665bc10d2d3a781d5ee87426d34d614b
moliveri1618 <lastiada1@gmail.com>
2025-04-25 11:41:23 +0100

    r

--- api/schemas/progetti.py
+++ api/schemas/progetti.py
@@ -3,13 +3,8 @@ from pydantic import BaseModel
 from datetime import datetime
 
 class ProgettiCreate(BaseModel):
-    nome_cliente: str
-    status: str
-    tecnico: str  # This field must be included
+    tecnico: str  
     stato: str
-    fornitori: str
-    centro_di_costo: str
-    indirizzo: str
     data_creazione: datetime
     importo: float
 
@@ -17,12 +12,7 @@ class ProgettiRead(ProgettiCreate):
     id: int
 
 class ProgettiUpdate(BaseModel):
-    nome_cliente: str | None = None
-    status: str | None = None
     tecnico: str | None = None
     stato: str | None = None
-    fornitori: str | None = None
-    centro_di_costo: str | None = None
-    indirizzo: str | None = None
     data_creazione: datetime | None = None
     importo: float | None = None
