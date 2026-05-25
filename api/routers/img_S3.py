96054471053b899d46ed1c4e6efcedda3299a3d7
moliveri1618 <lastiada1@gmail.com>
2026-05-25 21:22:27 +0200

    g

--- api/routers/img_S3.py
+++ api/routers/img_S3.py
@@ -1,116 +1,41 @@
-import sys
-import os
-from fastapi import FastAPI, Depends, Request, HTTPException
-from mangum import Mangum
-from fastapi.middleware.cors import CORSMiddleware
-from contextlib import asynccontextmanager
-import asyncio
-import httpx, io
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from fastapi.responses import StreamingResponse, JSONResponse
-
-
-if os.getenv("GITHUB_ACTIONS"):sys.path.append(os.path.dirname(__file__)) 
-from routers import (
-    progetti,
-    clienti,
-    fornitori,
-    progetto_fornitore_link,
-    getFiles,
-    prodotti,
-    notePrivate,
-    img_S3,
+from fastapi import (
+    APIRouter,
+    Depends,
+    HTTPException,
+    Query,
+    HTTPException,
+    Response,
+    status,
 )
+from botocore.exceptions import ClientError
+import boto3
 
-# from routers import clienti
-# from routers import fornitori
-# from routers import progetto_fornitore_link
-# from routers import getFiles
-from dependecies import create_db_and_tables, verify_cognito_token
+router = APIRouter()
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await asyncio.to_thread(create_db_and_tables)
-    yield
 
-app = FastAPI(lifespan=lifespan)
-handler = Mangum(app=app)
+KB = 1024
+MB = 1024 * KB
+MAX_EXPIRES = 7 * 24 * 3600
 
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://staging.d1z7mkjg7hq21f.amplifyapp.com"
-    ],
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
-    allow_headers=["Content-Type", "Authorization"], 
-)
-
-app.include_router(
-    progetti.router, 
-    prefix="/progetti", 
-    tags=["progetti"]
-)
-
-app.include_router(
-    clienti.router, 
-    prefix="/clienti", 
-    tags=["clienti"]
-)
-
-app.include_router(
-    fornitori.router, 
-    prefix="/fornitori", 
-    tags=["fornitori"]
-)
-
-app.include_router(
-    progetto_fornitore_link.router, 
-    prefix="/progetti-fornitori", 
-    tags=["progetti-fornitori"]
-)
-
-app.include_router(
-    getFiles.router, 
-    prefix="/getFiles", 
-    tags=["getFiles"]
-)
-
-app.include_router(
-    prodotti.router, 
-    prefix="/prodotti", 
-    tags=["prodotti"]
-)
-
-app.include_router(
-    notePrivate.router, 
-    prefix="/notePrivate", 
-    tags=["notePrivate"]
-)
-
-app.include_router(
-    img_S3.router,
-    prefix="/img_S3",
-    tags=["img_S3"]
-)
-
-@app.get("/")
-def root():
-    return {"message": "FastAPI test client is running"}
+SUPPORTED_FILE_TYPES = {
+    "image/png": "png",
+    "image/jpeg": "jpg",
+    "application/pdf": "pdf",
+}
+AWS_BUCKET = "images1files1bucket"
+AWS_REGION = "eu-north-1"  # keep in sync with your bucket region
+s3_client = boto3.client("s3", region_name=AWS_REGION)
+ALLOWED_PREFIXES = ["Gestionale/", "iMax/"]
 
 
-# #############################################
-# ########## Generate PRE Signed URL ##########
-# ########## Upload  ------ Download ##########
-# #############################################
+#############################################
+########## Generate PRE Signed URL ##########
+########## Upload  ------ Download ##########
+#############################################
 
 
-# @app.get("/presign-upload")
+# @router.get("/presign-upload")
 # async def presign_upload(
 #     key: str,
 #     expires: int = 60,
@@ -218,4 +143,4 @@ def root():
 #         raise HTTPException(status_code=500, detail="Failed to delete file")
 
 
-# # --- end block ----------------------------------------------------------------
+# --- end block ----------------------------------------------------------------
