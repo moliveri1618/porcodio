ec1bd295a67043b2d389e00252b9cd2f30b49a8f
moliveri1618 <lastiada1@gmail.com>
2026-06-23 18:34:10 +0200

    rrt

--- api/routers/scheda_tecnica_pezzo.py
+++ api/routers/scheda_tecnica_pezzo.py
@@ -3,6 +3,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from typing import List
 from sqlmodel import Session, select
+from sqlalchemy import delete
 
 from models.scheda_tecnica_pezzo import SchedaTecnicaPezzo
 from models.scheda_tecnica_schema import SchedaTecnicaSchema
@@ -20,25 +21,32 @@ from dependecies import get_db
 router = APIRouter()
 
 
-# Create
-@router.post("", response_model=SchedaTecnicaPezzoRead, status_code=201)
-def create_scheda_tecnica_pezzo(
-    scheda: SchedaTecnicaPezzoCreate,
-    db: Session = Depends(get_db),
-):
-    db_scheda = SchedaTecnicaPezzo(**scheda.dict())
-    db.add(db_scheda)
-    db.commit()
-    db.refresh(db_scheda)
-    return db_scheda
+# # Create
+# @router.post("", response_model=SchedaTecnicaPezzoRead, status_code=201)
+# def create_scheda_tecnica_pezzo(
+#     scheda: SchedaTecnicaPezzoCreate,
+#     db: Session = Depends(get_db),
+# ):
+#     db_scheda = SchedaTecnicaPezzo(**scheda.dict())
+#     db.add(db_scheda)
+#     db.commit()
+#     db.refresh(db_scheda)
+#     return db_scheda
 
 
+# Upsert One
 @router.post("/bulk/from-schede/{progetto_id}")
 def save_schede_tecniche_from_frontend(
     progetto_id: int,
     schede_tecniche: dict,
     db: Session = Depends(get_db),
 ):
+
+    # delete old values first
+    db.exec(
+        delete(SchedaTecnicaPezzo).where(SchedaTecnicaPezzo.progetto_id == progetto_id)
+    )
+    
     new_rows = []
 
     for scheda_wrapper in schede_tecniche.values():
@@ -68,6 +76,7 @@ def save_schede_tecniche_from_frontend(
     return {"created": len(new_rows)}
 
 
+# Get one
 @router.get("/by-progetto/{progetto_id}")
 def get_schede_tecniche_by_progetto(
     progetto_id: int,
@@ -167,22 +176,6 @@ def get_schede_tecniche_by_progetto(
     return final_result
 
 
-# @router.post("/bulk", response_model=List[SchedaTecnicaPezzoRead], status_code=201)
-# def create_schede_tecniche_pezzi_bulk(
-#     pezzi: List[SchedaTecnicaPezzoCreate],
-#     db: Session = Depends(get_db),
-# ):
-#     db_pezzi = [SchedaTecnicaPezzo(**pezzo.model_dump()) for pezzo in pezzi]
-
-#     db.add_all(db_pezzi)
-#     db.commit()
-
-#     for pezzo in db_pezzi:
-#         db.refresh(pezzo)
-
-#     return db_pezzi
-
-
 # Get all
 @router.get("", response_model=List[SchedaTecnicaPezzoRead])
 def read_schede_tecniche_pezzi(db: Session = Depends(get_db)):
@@ -190,54 +183,54 @@ def read_schede_tecniche_pezzi(db: Session = Depends(get_db)):
     return schede
 
 
-# Get one
-@router.get("/{scheda_id}", response_model=SchedaTecnicaPezzoRead)
-def read_scheda_tecnica_pezzo(
-    scheda_id: int,
-    db: Session = Depends(get_db),
-):
-    scheda = db.get(SchedaTecnicaPezzo, scheda_id)
+# # Get one
+# @router.get("/{scheda_id}", response_model=SchedaTecnicaPezzoRead)
+# def read_scheda_tecnica_pezzo(
+#     scheda_id: int,
+#     db: Session = Depends(get_db),
+# ):
+#     scheda = db.get(SchedaTecnicaPezzo, scheda_id)
 
-    if not scheda:
-        raise HTTPException(status_code=404, detail="Scheda tecnica pezzo not found")
+#     if not scheda:
+#         raise HTTPException(status_code=404, detail="Scheda tecnica pezzo not found")
 
-    return scheda
-
-
-# Put
-@router.put("/{scheda_id}", response_model=SchedaTecnicaPezzoRead)
-def update_scheda_tecnica_pezzo(
-    scheda_id: int,
-    scheda_update: SchedaTecnicaPezzoUpdate,
-    db: Session = Depends(get_db),
-):
-    scheda = db.get(SchedaTecnicaPezzo, scheda_id)
-
-    if not scheda:
-        raise HTTPException(status_code=404, detail="Scheda tecnica pezzo not found")
-
-    update_data = scheda_update.dict(exclude_unset=True)
-
-    for key, value in update_data.items():
-        setattr(scheda, key, value)
-
-    db.add(scheda)
-    db.commit()
-    db.refresh(scheda)
-
-    return scheda
+#     return scheda
 
 
-# Delete
-@router.delete("/{scheda_id}", status_code=204)
-def delete_scheda_tecnica_pezzo(
-    scheda_id: int,
-    db: Session = Depends(get_db),
-):
-    scheda = db.get(SchedaTecnicaPezzo, scheda_id)
+# # Put
+# @router.put("/{scheda_id}", response_model=SchedaTecnicaPezzoRead)
+# def update_scheda_tecnica_pezzo(
+#     scheda_id: int,
+#     scheda_update: SchedaTecnicaPezzoUpdate,
+#     db: Session = Depends(get_db),
+# ):
+#     scheda = db.get(SchedaTecnicaPezzo, scheda_id)
 
-    if not scheda:
-        raise HTTPException(status_code=404, detail="Scheda tecnica pezzo not found")
+#     if not scheda:
+#         raise HTTPException(status_code=404, detail="Scheda tecnica pezzo not found")
 
-    db.delete(scheda)
-    db.commit()
+#     update_data = scheda_update.dict(exclude_unset=True)
+
+#     for key, value in update_data.items():
+#         setattr(scheda, key, value)
+
+#     db.add(scheda)
+#     db.commit()
+#     db.refresh(scheda)
+
+#     return scheda
+
+
+# # Delete
+# @router.delete("/{scheda_id}", status_code=204)
+# def delete_scheda_tecnica_pezzo(
+#     scheda_id: int,
+#     db: Session = Depends(get_db),
+# ):
+#     scheda = db.get(SchedaTecnicaPezzo, scheda_id)
+
+#     if not scheda:
+#         raise HTTPException(status_code=404, detail="Scheda tecnica pezzo not found")
+
+#     db.delete(scheda)
+#     db.commit()
